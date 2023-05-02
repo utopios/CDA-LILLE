ab1d6b5742f45085a333bc584b0bdbef0fef3918
ihab <christophe.delory@labom2iformation.fr>
2023-05-02 13:49:18 +0200

    debut de correction exercice 1

--- cours-jdbc/src/main/java/exercice1/Student.java
+++ cours-jdbc/src/main/java/exercice1/Student.java
@@ -1,5 +1,12 @@
 package exercice1;
 
+import org.example.util.DataBaseManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
@@ -7,13 +14,35 @@ public class Student {
     private String firstName;
     private String lastName;
     private String dateDegree;
-    private String classNumber;
+    private int classNumber;
 
-    public Student(String firstName, String lastName, String dateDegree, String classNumber) {
+    private static String request;
+
+    private static PreparedStatement statement;
+
+    private static Connection connection;
+
+
+
+    public Student(String firstName, String lastName, String dateDegree, int classNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateDegree = dateDegree;
         this.classNumber = classNumber;
+    }
+
+    public Student(int id, String firstName, String lastName, String dateDegree, int classNumber) {
+        this(firstName, lastName, dateDegree, classNumber);
+        this.id = id;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -40,27 +69,59 @@ public class Student {
         this.dateDegree = dateDegree;
     }
 
-    public String getClassNumber() {
+    public int getClassNumber() {
         return classNumber;
     }
 
-    public void setClassNumber(String classNumber) {
+    public void setClassNumber(int classNumber) {
         this.classNumber = classNumber;
     }
 
-    public boolean save() {
-        return false;
+    public boolean save() throws SQLException {
+        request = "INSERT INTO student (first_name, last_name, date_degree, class_number) values (?,?,?,?)";
+        connection = new DataBaseManager().getConnection();
+        statement = connection.prepareStatement(request);
+        statement.setString(1, getFirstName());
+        statement.setString(2, getLastName());
+        statement.setString(3, getDateDegree());
+        statement.setInt(4, getClassNumber());
+        int rowNb =statement.executeUpdate();
+        return rowNb > 0;
     }
 
-    public boolean delete() {
-        return false;
+    public boolean delete() throws SQLException {
+        request = "DELETE FROM student where id = ?";
+        connection = new DataBaseManager().getConnection();
+        statement = connection.prepareStatement(request);
+        statement.setInt(1, getId());
+        int rowNb =statement.executeUpdate();
+        return rowNb > 0;
     }
 
-    public List<Student> getAll() {
-        return null;
+    public List<Student> getAll() throws SQLException {
+        List<Student> result = new ArrayList<>();
+        request = "SELECT * FROM student";
+        connection = new DataBaseManager().getConnection();
+        statement = connection.prepareStatement(request);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            Student s = new Student(resultSet.getInt("id"),resultSet.getString("first_name"), resultSet.getString("last_name"), resultSet.getString("date_degree"), resultSet.getInt("class_number"));
+            result.add(s);
+        }
+        return result;
     }
 
-    public List<Student> getByClass(int classNumber) {
-        return null;
+    public List<Student> getByClass(int classNumber) throws SQLException {
+        List<Student> result = new ArrayList<>();
+        request = "SELECT * FROM student where class_number = ?";
+        connection = new DataBaseManager().getConnection();
+        statement = connection.prepareStatement(request);
+        statement.setInt(1, classNumber);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            Student s = new Student(resultSet.getInt("id"),resultSet.getString("first_name"), resultSet.getString("last_name"), resultSet.getString("date_degree"), resultSet.getInt("class_number"));
+            result.add(s);
+        }
+        return result;
     }
 }
