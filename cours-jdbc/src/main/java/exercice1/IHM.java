039538c27517531cc18897685968e24d20d1c448
ihab <christophe.delory@labom2iformation.fr>
2023-05-02 12:22:24 +0200

    debut de correction exercice 1

--- cours-jdbc/src/main/java/exercice1/IHM.java
+++ cours-jdbc/src/main/java/exercice1/IHM.java
@@ -1,4 +1,29 @@
 package exercice1;
 
 public class IHM {
+
+    public void start() {
+
+    }
+
+    private void menu() {
+
+    }
+
+    private void addStudentAction() {
+
+    }
+
+    private void getAllStudentsAction() {
+
+    }
+
+    private void getStudentsByClassIdAction() {
+
+    }
+
+    private void deleteStudentAction() {
+
+    }
 }
+
