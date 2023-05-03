65d2db7256ac59eae5aef550ec8b9c4ae8b8b7b1
ihab <christophe.delory@labom2iformation.fr>
2023-05-03 16:24:21 +0200

    correction banque jdbc

--- cours-jdbc/src/main/java/exercice3/BankAccount.java
+++ cours-jdbc/src/main/java/exercice3/BankAccount.java
@@ -1,10 +1,17 @@
 package exercice3;
 
+import com.mysql.cj.xdevapi.Client;
+import org.example.util.DataBaseManager;
+
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class BankAccount extends BaseJDBC {
     private int id;
     private int customerId;
+
+    private Customer customer;
 
     private List<Operation> operations;
 
@@ -35,20 +42,60 @@ public class BankAccount extends BaseJDBC {
         this.id = id;
     }
 
-    public boolean save() {
+    public boolean save() throws SQLException {
+        request = "INSERT INTO bank_account (total_amount, customer_id) values (?,?)";
+        connection = new DataBaseManager().getConnection();
+        statement = connection.prepareStatement(request, Statement.RETURN_GENERATED_KEYS);
+        statement.setDouble(1, getTotalAmount());
+        statement.setInt(2, getCustomerId());
+        int nbRow = statement.executeUpdate();
+        resultSet = statement.getGeneratedKeys();
+        if(resultSet.next()) {
+            this.id = resultSet.getInt(1);
+        }
+        return nbRow == 1;
+    }
+    public boolean update() throws SQLException {
+        request = "UPDATE bank_account set total_amount = ? where id = ?";
+        connection = new DataBaseManager().getConnection();
+        statement = connection.prepareStatement(request);
+        statement.setDouble(1, getTotalAmount());
+        statement.setInt(2, getId());
+        int nbRow = statement.executeUpdate();
+        return nbRow == 1;
+    }
+    public boolean makeDeposit(Operation operation) throws SQLException {
+        if(operation.getAmount() > 0 && operation.save()) {
+            operations.add(operation);
+            totalAmount += operation.getAmount();
+            return update();
+        }
         return false;
     }
-    public boolean update() {
+    public boolean makeWithDrawl(Operation operation) throws SQLException {
+        if(operation.getAmount() < 0 && getTotalAmount() >= operation.getAmount() && operation.save()) {
+            operations.add(operation);
+            totalAmount += operation.getAmount();
+            return update();
+        }
         return false;
     }
-    public boolean makeDeposit(Operation operation) {
-        return false;
-    }
-    public boolean makeWithDrawl(Operation operation) {
-        return false;
-    }
-    public static BankAccount getById(int id) {
-        return null;
+    public static BankAccount getById(int id) throws SQLException {
+        BankAccount bankAccount = null;
+        request = "SELECT * FROM bank_account where id = ?";
+        connection = new DataBaseManager().getConnection();
+        statement = connection.prepareStatement(request);
+        statement.setInt(1, id);
+        resultSet = statement.executeQuery();
+        if(resultSet.next()) {
+            bankAccount = new BankAccount(resultSet.getInt("id"),
+                    resultSet.getInt("customer_id"),
+                    resultSet.getDouble("total_amount")
+            );
+        }
+        bankAccount.customer = Customer.getById(bankAccount.getId());
+        bankAccount.operations = Operation.getAllByAccountId(bankAccount.getId());
+        return bankAccount;
     }
 
 
