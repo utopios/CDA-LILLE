58b736656f8219b8abf178708b6c9292570aefb1
ihab <christophe.delory@labom2iformation.fr>
2023-05-11 09:44:34 +0200

    singleton

---
+++ cours-jdbc/src/main/java/designpattern/Singleton.java
@@ -0,0 +1,16 @@
+package designpattern;
+
+//Singleton no thread safe
+public class Singleton {
+
+    private static Singleton instance = null;
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if(instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+}
