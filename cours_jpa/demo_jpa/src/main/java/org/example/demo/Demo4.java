e5d6091ddfe215d16b95b5ea3cd25611ac62e986
ihab <christophe.delory@labom2iformation.fr>
2023-05-17 10:10:05 +0200

    nouvelle demo one to many

--- cours_jpa/demo_jpa/src/main/java/org/example/demo/Demo4.java
+++ cours_jpa/demo_jpa/src/main/java/org/example/demo/Demo4.java
@@ -1,8 +1,5 @@
 package org.example.demo;
 
-import org.example.entity.oneToMany.Group;
-import org.example.entity.oneToMany.User;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,22 +9,7 @@ public class Demo4 {
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_demo");
     public static void main() {
         EntityManager em = emf.createEntityManager();
-     /*   em.getTransaction().begin();
 
-        User user = new User();
-
-        user.setUsername("toto");
-        user.setPassword("1234");
-
-        Group group = new Group();
-
-        group.setName("CDA Lille");
-        group.getUsers().add(user);
-
-        em.persist(user);
-        em.persist(group);
-
-        em.getTransaction().commit();*/
 
         em.close();
         emf.close();
