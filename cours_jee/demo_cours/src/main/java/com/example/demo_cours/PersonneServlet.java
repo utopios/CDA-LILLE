a1575bc968b74fe72904786b84bf6eacb46edc6d
ihab <christophe.delory@labom2iformation.fr>
2023-06-05 16:04:31 +0200

    get parameter

--- cours_jee/demo_cours/src/main/java/com/example/demo_cours/PersonneServlet.java
+++ cours_jee/demo_cours/src/main/java/com/example/demo_cours/PersonneServlet.java
@@ -21,15 +21,38 @@ public class PersonneServlet extends HttpServlet {
         personneList.add(new Personne("minet", "titi"));
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>Liste des personnes</h1>");
-        for(Personne p : personneList) {
-            out.println("<div>"+p.getNom() +" " + p.getPrenom() + "</div>");
+        if(request.getParameter("nom") != null) {
+            String nom = request.getParameter("nom");
+            out.println("<div>Une seule personne avec nom : "+nom+"</div>");
+        }else {
+            out.println("<h1>erreur de données</h1>");
         }
+
+        out.println("</body></html>");
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        if(request.getParameter("id") != null) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            out.println("<div>Une seule personne avec id : "+id+"</div>");
+        }else {
+            out.println("<h1>Liste des personnes</h1>");
+            for(Personne p : personneList) {
+                out.println("<div>"+p.getNom() +" " + p.getPrenom() + "</div>");
+            }
+        }
+
         out.println("</body></html>");
     }
 
