3eebfaf8f63b89e8c8f8431b5989ee166a7854e7
ihab <christophe.delory@labom2iformation.fr>
2023-06-06 09:43:32 +0200

    avec lib

--- cours_jee/demo_cours/src/main/java/com/example/demo_cours/RedirectionServlet.java
+++ cours_jee/demo_cours/src/main/java/com/example/demo_cours/RedirectionServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "redirection", value = "/redirection-servlet")
 public class RedirectionServlet extends HttpServlet {
@@ -16,8 +18,11 @@ public class RedirectionServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Personne> personnes = new ArrayList<>();
         Personne personne = new Personne("toto", "tata");
+        personnes.add(personne);
         request.setAttribute("personne", personne);
+        request.setAttribute("personnes", personnes);
         request.getRequestDispatcher("redirection.jsp").forward(request, response);
     }
 
--- cours_jee/demo_cours/src/main/webapp/redirection.jsp
+++ cours_jee/demo_cours/src/main/webapp/redirection.jsp
@@ -5,6 +5,7 @@
   Time: 09:08
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -12,11 +13,18 @@
 </head>
 <body>
 <div>
-    <h1>Redirection après request</h1>
-    <div>
-        Nom : ${personne.getNom()}
-        Prénom: ${personne.getPrenom()}
-    </div>
+<%--    <h1>Redirection après request</h1>--%>
+<%--    <div>--%>
+<%--        Nom : ${personne.getNom()}--%>
+<%--        Prénom: ${personne.getPrenom()}--%>
+<%--    </div>--%>
+    <h2>Liste des personnes</h2>
+    <c:forEach items="${personnes}" var="personne">
+        <div>
+            Nom : ${personne.getNom()}
+            Prénom: ${personne.getPrenom()}
+        </div>
+    </c:forEach>
 </div>
 </body>
 </html>
