4b18cda7859eea424af7668be321cf7e7ac9ef49
ihab <christophe.delory@labom2iformation.fr>
2023-06-07 16:46:45 +0200

    suite de la correction

--- cours_jee/correctionpatient/src/main/java/com/example/correctionpatient/controller/PatientServlet.java
+++ cours_jee/correctionpatient/src/main/java/com/example/correctionpatient/controller/PatientServlet.java
@@ -1,11 +1,13 @@
 package com.example.correctionpatient.controller;
 
 import com.example.correctionpatient.entity.Patient;
+import com.example.correctionpatient.util.HibernateSession;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
 
 import java.io.IOException;
 
@@ -19,7 +21,11 @@ public class PatientServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        Patient patient = new Patient();
+        patient.setName("toto");
+        Session session = HibernateSession.getSessionFactory().openSession();
+        session.save(patient);
+        session.close();
         request.getRequestDispatcher(VIEW_PATH + "/patients.jsp").forward(request, response);
     }
 }
