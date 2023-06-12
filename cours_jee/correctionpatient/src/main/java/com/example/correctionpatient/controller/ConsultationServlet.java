365d839e55433a49ae045ecf301850b1ac42c482
ihab <christophe.delory@labom2iformation.fr>
2023-06-12 11:17:34 +0200

    suite correction patient

--- cours_jee/correctionpatient/src/main/java/com/example/correctionpatient/controller/ConsultationServlet.java
+++ cours_jee/correctionpatient/src/main/java/com/example/correctionpatient/controller/ConsultationServlet.java
@@ -1,8 +1,10 @@
 package com.example.correctionpatient.controller;
 
+import com.example.correctionpatient.entity.Consultation;
 import com.example.correctionpatient.service.ConsultationService;
 import com.example.correctionpatient.util.Definition;
 import com.example.correctionpatient.util.HibernateSession;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,11 +17,19 @@ public class ConsultationServlet extends HttpServlet {
     private ConsultationService consultationService;
 
     public void init() {
-
+        consultationService = new ConsultationService(HibernateSession.getSessionFactory());
     }
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getParameter("id") != null && !request.getParameter("id").equals("")) {
+            int consultationId = Integer.parseInt(request.getParameter("id"));
+            Consultation consultation = consultationService.getByIdConsultation(consultationId);
+            request.setAttribute("consultation", consultation);
+            request.getRequestDispatcher(Definition.VIEW_PATH + "/consultation.jsp").forward(request, response);
+        }
+    }
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        consultationService = new ConsultationService(HibernateSession.getSessionFactory());
+
         if(request.getParameter("patientId") != null && !request.getParameter("patientId").equals("")) {
             int patientId = Integer.parseInt(request.getParameter("patientId"));
             if(consultationService.createConsultation(patientId)) {
---
+++ cours_jee/correctionpatient/src/main/webapp/WEB-INF/views/consultation.jsp
@@ -0,0 +1,41 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrateur
+  Date: 12/06/2023
+  Time: 11:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Consultation ${consultation.getId()}</title>
+  <jsp:include page="../includes/head.jsp" ></jsp:include>
+</head>
+<body>
+<jsp:include page="../includes/header.jsp" ></jsp:include>
+<div>
+  <h1>Consultation : ${consultation.getId()}, ${consultation.getDateConsultation()}</h1>
+</div>
+<div>
+  <h2>Fiche soins</h2>
+  <:c:if test="${consultation.getFicheSoins() != null}">
+    <div>
+      ${consultation.getFicheSoins().getContent()}
+    </div>
+  </:c:if>
+  <:c:if test="${consultation.getFicheSoins() == null}">
+    <form action="Soins" method="post">
+      <input type="hidden" name="consultationId" value="${consultation.getId()}">
+      <div>
+        <label>Contenu</label>
+        <textarea name="content"></textarea>
+      </div>
+      <div>
+        <button type="submit">Valider</button>
+      </div>
+    </form>
+  </:c:if>
+</div>
+</body>
+</html>
