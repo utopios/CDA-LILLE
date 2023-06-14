260db93308e8fe5548292b8adcc86147ad93364b
ihab <christophe.delory@labom2iformation.fr>
2023-06-14 14:23:41 +0200

    exercice 5 bis

---
+++ cours-test/demo_base_test/src/main/java/org/example/ConditionException.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class ConditionException extends Exception{
+}
---
+++ cours-test/demo_base_test/src/main/java/org/example/Voiture.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Voiture {
+    private int rentDueIn;
+    private int condition;
+    private String type;
+}
