d387a12fb43bc4b00a89c96ceece844c33bbb4b7
ihab <christophe.delory@labom2iformation.fr>
2023-06-28 15:27:07 +0200

    login et inscription fait

--- correction-petite-annonce/src/main/java/com/example/correctionpetiteannonce/service/CategoryService.java
+++ correction-petite-annonce/src/main/java/com/example/correctionpetiteannonce/service/CategoryService.java
@@ -4,6 +4,8 @@ import com.example.correctionpetiteannonce.entity.Ad;
 import com.example.correctionpetiteannonce.entity.Category;
 import com.example.correctionpetiteannonce.exception.CategoryExistException;
 import com.example.correctionpetiteannonce.exception.EmptyFieldsException;
+import com.example.correctionpetiteannonce.exception.NotAdminException;
+import com.example.correctionpetiteannonce.exception.NotSignInException;
 import com.example.correctionpetiteannonce.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,16 +20,25 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public boolean saveCategory(String title) throws CategoryExistException, EmptyFieldsException {
-        if(title != null) {
-            if(!categoryRepository.existsCategoryByTitle(title)) {
-                Category category = Category.builder().title(title).build();
-                categoryRepository.save(category);
-                return category.getId() > 0;
+    @Autowired
+    private LoginService loginService;
+
+    public boolean saveCategory(String title) throws CategoryExistException, EmptyFieldsException, NotAdminException, NotSignInException {
+        if(loginService.isLogged()) {
+            if (loginService.isAdmin()) {
+                if(title != null) {
+                    if(!categoryRepository.existsCategoryByTitle(title)) {
+                        Category category = Category.builder().title(title).build();
+                        categoryRepository.save(category);
+                        return category.getId() > 0;
+                    }
+                    throw new CategoryExistException();
+                }
+                throw EmptyFieldsException.with("title");
             }
-            throw new CategoryExistException();
+            throw new NotAdminException();
         }
-        throw EmptyFieldsException.with("title");
+        throw new NotSignInException();
     }
 
     public List<Category> getCategories() {
