98bfece248af3724b75baeb772cd290f77fba647
ihab <christophe.delory@labom2iformation.fr>
2023-07-04 16:05:51 +0200

    dto correction

--- demo-rest/src/main/java/com/example/demorest/controller/EmployeeController.java
+++ demo-rest/src/main/java/com/example/demorest/controller/EmployeeController.java
@@ -9,10 +9,11 @@ import com.example.demorest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -36,9 +37,45 @@ public class EmployeeController {
 
     }
 
+    @GetMapping("/employees")
+    public ResponseEntity<List<EmployeeDto>> getAllEmployeeDto(){
+       List <EmployeeDto> employeeDtos = new ArrayList<>();
+       for (Employee employee : employeeService.findAll()){
+           employeeDtos.add(employeeToEmployeDto.convert(employee));
+       }
+       return new ResponseEntity<>(employeeDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<EmployeeDto> getEmploye(@PathVariable Integer id){
+        Optional<Employee> employee = employeeService.findById(id);
+        if(employee.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<EmployeeDto>(employeeToEmployeDto.convert(employee.get()), HttpStatus.OK);
+
+    }
 
 
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@RequestBody EmployeeDto employeeDto, @PathVariable Integer id){
+        employeeDto.setId(id);
+        Employee employee =  employeeService.save(employeeDtoToEmployee.convert(employeeDto));
 
+        return new ResponseEntity<>(employeeToEmployeDto.convert(employee),HttpStatus.ACCEPTED);
 
+    }
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<EmployeeDto> deleteEmployeeById(@PathVariable Integer id) {
+        try {
+            employeeService.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+
+    }
 }
