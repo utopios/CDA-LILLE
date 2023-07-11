59bac29fe2488f29b3a63c5b7d378e1b7deed534
ihab <christophe.delory@labom2iformation.fr>
2023-07-11 15:09:50 +0200

    add ems-correction

---
+++ ems-backend/src/main/java/net/javaguides/ems/mapper/DepartmentMapper.java
@@ -0,0 +1,25 @@
+package net.javaguides.ems.mapper;
+
+import net.javaguides.ems.dto.DepartmentDto;
+import net.javaguides.ems.entity.Department;
+
+public class DepartmentMapper {
+
+
+    public static DepartmentDto mapToDepartmentDto(Department department){
+        return new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription()
+        );
+    }
+
+
+    public static Department mapToDepartment(DepartmentDto departmentDto){
+        return new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription()
+        );
+    }
+}
